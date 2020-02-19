3aaf819864bfda6798b98662e08f588347d4aa00
thisal.man@gmail.com <thisal.man@gmail.com>
2020-02-19 16:19:45 +0530

    Merge branch 'i' of /Users/Thisal/Downloads/FYPUI 7_20012020_CardAnimationsDoneANDTrumpAddedANDWishesDoneDialogColorChangeDrawAnimationDoneComputerPlayerSelectingTrumpAdded&scoreBoardClassAdded/FYPUI 7_17012020_CardAnimationsDoneANDTrumpAddedANDWishesDoneDialogColorChangeDrawAnimationDone/FYPUI/Hath_Wasi with conflicts.

--- app/src/main/java/com/example/fypui/instructions_page.java
+++ app/src/main/java/com/example/fypui/instructions_page.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 public class instructions_page extends AppCompatActivity {
 
@@ -18,5 +19,6 @@ public class instructions_page extends AppCompatActivity {
         getSupportActionBar().hide();
 
         setContentView(R.layout.activity_instructions_page);
+
     }
 }
